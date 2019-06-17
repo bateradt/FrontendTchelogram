f5aff7a4f41c6d80e1b744ab4df2fdfda32dd4d3
bateradt <marcelofscarpim@gmail.com>
2019-06-17 20:14:09 -0300

    Configuração para acessar o backend do Heroku

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -5,6 +5,7 @@ import io from 'socket.io-client';
 
 //Estilo
 import "./Feed.css";
+//import {PostList} from './FeedStyles'; //componente estilizado
 
 //Icones
 import more from "../assets/more.svg";
@@ -25,16 +26,16 @@ class Feed extends Component {
   };
 
   registerToSocket = () => {
-      const socket = io('http://localhost:3002');
+      const socket = io('https://tchelogram-backend.herokuapp.com');//io('http://localhost:3002');
 
       socket.on('post', newPost =>{
-        this.setState({feed: [newPost, ...this.state.feed]});
+        this.setState({feed: [newPost, ...this.state.feed]}); 
       });
 
       socket.on('like', likedPost => {
           this.setState({
               feed: this.state.feed.map(post => 
-                post._id == likedPost._id ? likedPost : post
+                post._id === likedPost._id ? likedPost : post
                 )
           })
       })
@@ -48,6 +49,7 @@ class Feed extends Component {
   render() {
     return (
       <section id="post-list">
+      {/* //<PostList> */}
         {this.state.feed.map(post => (
           <article key={post._id}>
             <header>
@@ -58,7 +60,7 @@ class Feed extends Component {
               <img src={more} alt="Mais" />
             </header>
 
-            <img src={`http://localhost:3002/files/${post.image}`} alt="" />
+            <img src={`https://tchelogram-backend.herokuapp.com/files/${post.image}`} alt="" />
 
             <footer>
               <div className="actions">
@@ -76,7 +78,8 @@ class Feed extends Component {
             </footer>
           </article>
         ))}
-      </section>
+      {/* //</PostList> */}
+     </section>
     );
   }
 }
